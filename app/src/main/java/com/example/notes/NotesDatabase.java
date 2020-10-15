24bc24d0865ff7c9b225a591c55ceb09c764b257
DESKTOP-OIV7B07\sstre <sstreltsov55@gmail.com>
2020-10-15 18:02:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notes/NotesDatabase.java
@@ -0,0 +1,27 @@
+package com.example.notes;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
+public abstract class NotesDatabase extends RoomDatabase {
+
+    private static NotesDatabase database;
+    private static String DB_NAME = "notes2.db";
+    private static final Object LOCK = new Object();
+
+
+    public static NotesDatabase getInstance(Context context){
+        synchronized (LOCK){
+            if (database == null){
+                database = Room.databaseBuilder(context,NotesDatabase.class, DB_NAME)
+                        .build();
+            }
+        }
+        return database;
+    }
+
+    public abstract NotesDao notesDao();
+}
